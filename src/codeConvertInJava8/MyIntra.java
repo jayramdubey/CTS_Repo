cfae5264783054889bb1da389c24d4ba02d73594
jayram dubey <jayram.dubey11@gmail.com>
2020-05-11 11:11:20 +0530

    refactoring

--- src/codeConvertInJava8/MyIntra.java
+++
@@ -1,7 +0,0 @@
-package codeConvertInJava8;
-
-import java.util.List;
-
-public interface MyIntra {
-	public  List<List<String>> groupAnagramsBestMethods();
-}
--- src/lembdaexpresions/MyInterface.java
+++
@@ -1,9 +0,0 @@
-package lembdaexpresions;
-
-import java.util.List;
-
-public interface MyInterface {
-	
-	public  List<List<String>> groupAnagramsBestMethods(String[] words);
-
-}
